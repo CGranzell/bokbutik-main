61b4dc91eb309da93c764b88a1b007fb1d634750
CGranzell <91188714+CGranzell@users.noreply.github.com>
2022-06-03 17:58:02 +0200

    börjat på footer

--- public/my-account.php
+++ public/my-account.php
@@ -1,5 +1,6 @@
 <?php
 require('../src/config.php');
+include('../public/layout/header.php');
 
 // Tar bort användarkonto
 if(isset($_POST['deleteAccountBtn'])) {
@@ -24,20 +25,6 @@ echo "</pre>";
 
 ?>
 
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  	<!-- Bootstrap CSS -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-
-  <title>Mina Sidor</title>
-</head>
-<body>
   <h1>Mina sidor</h1>
   <!-- Tabell över Användarens uppgifter -->
   <table class="table">
@@ -89,8 +76,7 @@ echo "</pre>";
         <?php endforeach ?>
   </tbody>
 </table>
+<?php 
 
-<!-- Bootstrap JS -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
-</body>
-</html>
+include('../public/layout/footer.php');
+?>
--- public/register-account.php
+++ public/register-account.php
@@ -1,6 +1,7 @@
 <?php
 
 require('../src/config.php');
+include('../public/layout/header.php');
 
 
 
@@ -15,15 +16,6 @@ require('../src/config.php');
 
 ?>
 
-
-<!DOCTYPE html>
-<html>
-<head>
-	<!-- Bootstrap CSS -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-	<title>Register </title>
-</head>
-<body>
   <h1>Register Here</h1>
 	<!-- Registrerings formulär -->
 <form>
@@ -77,8 +69,7 @@ require('../src/config.php');
 
 </form>
 
-<!-- Bootstrap JS -->
-		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
-  </body>
-</body>
-</html>
+<?php 
+
+include('../public/layout/footer.php');
+?>
