308c2fe9efbba9244531e205229d173fe681edb3
CGranzell <91188714+CGranzell@users.noreply.github.com>
2022-06-03 09:49:40 +0200

    sido struktur och html

--- public/login.php
+++ public/login.php
@@ -1,26 +1,46 @@
 <?php
 
 require('../src/config.php');
-echo "<pre>";
-print_r(PDO::getAvailableDrivers());
-echo "</pre>";
-
-$sql = "SELECT * FROM users";
-$statement = $dbconnect->query($sql);
-$users = $statement->fetchAll();
 
 
 ?>
 
 
+
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<title>Lo</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  	<!-- Bootstrap CSS -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+  <title>Log in</title>
 </head>
 <body>
-	<h1>Log in sida</h1>
-	<p>Hallå </p>
+ <h1>Log in</h1>
+	<!-- Inloggningsformulär -->
+  <form>
+	
+	<!-- Email -->
+  <div class="mb-3">
+    <label for="email" class="form-label">Email address</label>
+    <input type="email" class="form-control">
+  </div>
+	<!-- Password -->
+  <div class="mb-3">
+    <label for="password" class="form-label">Password</label>
+    <input type="password" class="form-control" id="password">
+  </div>
+  <!-- Login Btn -->
+  <input type="submit" class="btn btn-primary" name="loginBtn" value="Login">
 
+</form>
+
+
+  
+
+<!-- Bootstrap JS -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 </body>
 </html>
