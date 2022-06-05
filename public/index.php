76bc0aecfe9276c3f83d0cc18f22191839543105
Fredrik Johan Dahlbom <fredrik.dahlbom@cmeducations.se>
2022-06-05 14:22:16 +0200

    Försökte infoga footer

--- public/index.php
+++ public/index.php
@@ -2,7 +2,8 @@
 <?php
 
  // require('C:\MAMP\htdocs\eshop\bokbutik-main\src\dbconnect.php');
- require('../src/config.php');
+ require('../src/config.php'); //hänvisar till mappen ovanför
+ include('../public/layout/header.php');
  
 //READ PRODUCTS
 
@@ -20,27 +21,12 @@ $products = $stmt->fetchAll();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Shop page</title>
 	<!-- Bootstrap CSS -->
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+	
     <link rel="stylesheet" href="css/style.css">
 	
 </head>
 <body>
-<nav class="navbar-light" style="background-color: #e3f2fd;">
-<ul class="nav justify-content-center">
-  <li class="nav-item">
-    <a class="nav-link active" aria-current="page" href="#">Active</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">Link</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">Link</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-  </li>
-</ul>
-</nav>
+
 
     <p>
 	<h1 class="fredriks-huvudrubrik">Public page/Shop</h1>
@@ -70,31 +56,11 @@ $products = $stmt->fetchAll();
         </tbody>
 
     </table>
+    
+<?php 
 
-    <footer id="footer" class="mt-auto  footer">
-<ul class="nav justify-content-center">
-  <li class="nav-item">
-    <a class="nav-link active" aria-current="page" href="#">Active</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">Link</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">Link</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-  </li>
-</ul>
-
-</footer>
-
-<!-- Bootstrap JS för footer -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
-     		
-
-<!-- Bootstrap JS för header -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+include('../public/layout/footer.php');
+?>
 </body>
 </html>
 
