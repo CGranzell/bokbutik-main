0f866bb57570291eae4c9c42a78f49ef11aa3fce
Fredrik Johan Dahlbom <fredrik.dahlbom@cmeducations.se>
2022-06-05 13:29:55 +0200

    Infogar Bootstrap-header

--- public/index.php
+++ public/index.php
@@ -19,11 +19,31 @@ $products = $stmt->fetchAll();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Shop page</title>
+	<!-- Bootstrap CSS -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
 	
 </head>
 <body>
-	<h1>Public page/Shop</h1>
+<nav class="navbar-light" style="background-color: #e3f2fd;">
+<ul class="nav justify-content-center">
+  <li class="nav-item">
+    <a class="nav-link active" aria-current="page" href="#">Active</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+  </li>
+</ul>
+</nav>
+
+    <p>
+	<h1 class="fredriks-huvudrubrik">Public page/Shop</h1>
 	<p>
 	
 	<table id="fredriks-tbl">
@@ -52,7 +72,8 @@ $products = $stmt->fetchAll();
     </table>
      		
 
-<p>
+<!-- Bootstrap JS -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 </body>
 </html>
 
