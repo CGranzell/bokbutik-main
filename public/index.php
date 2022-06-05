42bc91bc69e2864546f310c68ce03a5c07f44d24
Fredrik Johan Dahlbom <fredrik.dahlbom@cmeducations.se>
2022-06-05 10:14:18 +0200

    länkar till css,lägger till metadata i head

--- public/index.php
+++ public/index.php
@@ -13,13 +13,18 @@ $products = $stmt->fetchAll();
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Shop</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shop page</title>
+    <link rel="stylesheet" href="css/style.css">
+	
 </head>
 <body>
 	<h1>Public page/Shop</h1>
 	<p>
 	
-	<table>
+	<table id="fredriks-tbl">
         <thead>
             <tr>
                 <th>Id</th>
