976838204514d87231c590da3ccc33ff69ac8822
Fredrik Johan Dahlbom <fredrik.dahlbom@cmeducations.se>
2022-06-06 11:23:59 +0200

    Infogar rätt <title> på min sida

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@
 
  // require('C:\MAMP\htdocs\eshop\bokbutik-main\src\dbconnect.php');
  require('../src/config.php'); //hänvisar till mappen ovanför
- include('../public/layout/header.php');
+ 
  
 //READ PRODUCTS
 
@@ -19,12 +19,39 @@ $products = $stmt->fetchAll();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?=CSS_PATH . 'style.css'?>">
+	<link rel="stylesheet" href="css/style.css">
     <title>Shop page</title>
-		
-    <link rel="stylesheet" href="css/style.css">
 	
 </head>
-<body>
+
+
+<body id="body">
+  
+<nav class="navbar-light" style="background-color: #e3f2fd;">
+<ul class="nav justify-content-center">
+  <li class="nav-item">
+    <a class="nav-link active" aria-current="page" href="#">Active</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="./register-account.php">Register</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="./login-account.php">Login</a>
+  </li>
+ 
+</ul>
+</nav>
+
+
+
 
 
     <p>
