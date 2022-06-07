4d38c4d86b94ebf01ae6f0dd4ee1275a51a149fe
Fredrik Johan Dahlbom <fredrik.dahlbom@cmeducations.se>
2022-06-07 15:38:22 +0200

    ändrade min title

--- public/index.php
+++ public/index.php
@@ -46,6 +46,19 @@ $products = $stmt->fetchAll();
 </nav>
 -->
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  
+	<link rel="stylesheet" href="css/style.css">
+  <title>All products</title>
+	
+</head>
+<body>
+
     <p>
 	<h1 class="fredriks-huvudrubrik">Shop page</h1>
 	<p>
