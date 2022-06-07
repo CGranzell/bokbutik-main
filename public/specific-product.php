85f11fad8e45f06c7707771b85a92b4123616f59
Fredrik Johan Dahlbom <fredrik.dahlbom@cmeducations.se>
2022-06-07 21:51:44 +0200

    change

--- public/specific-product.php
+++ public/specific-product.php
@@ -12,8 +12,23 @@ $products = $stmt->fetchAll();
 
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  
+	<link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="<?=CSS_PATH . 'style.css'?>">
+  <title>Specific product</title>
+	
+</head>
 
-  <html>
+
+<body>
+
+
         <p>
       <h1 class="fredriks-huvudrubrik">Shop page</h1>
       <h2 class="fredriks-underrubrik"> Specific product </h2>
