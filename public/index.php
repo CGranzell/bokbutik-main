890f730fea051fc5864226c12d4dac7ab7305b25
Fredrik Johan Dahlbom <fredrik.dahlbom@cmeducations.se>
2022-06-07 20:34:18 +0200

    tog bort onödig kod

--- public/index.php
+++ public/index.php
@@ -6,45 +6,8 @@ include('./layout/header.php');
 $stmt = $dbconnect->query("SELECT * FROM products"); 	
 $products = $stmt->fetchAll(); 	
 ?>
-<!--
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-  <link rel="stylesheet" href="<?=CSS_PATH . 'style.css'?>">  
+
 	
-    <title>Shop page</title>
-    
-</head>
-
-
-<body id="body">
-
-<nav class="navbar-light" style="background-color: #e3f2fd;">
-<ul class="nav justify-content-center">
-  <li class="nav-item">
-    <a class="nav-link active" aria-current="page" href="#">Active</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">Link</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">Link</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="./register-account.php">Register</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="./login-account.php">Login</a>
-  </li>
-
-</ul>
-</nav>
--->
 
 <!DOCTYPE html>
 <html lang="en">
