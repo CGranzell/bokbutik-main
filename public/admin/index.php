264bfaeef57f809726f1a4f421d0753a587ae7e7
Chakal <chakirrasta@gmail.com>
2022-06-08 20:26:58 +0300

    bug in update-product fixed

--- public/admin/index.php
+++ public/admin/index.php
@@ -1,7 +1,8 @@
 <?php
-require('C:\MAMP\htdocs\bokbutik-main\src\dbconnect.php');
+//require('C:\MAMP\htdocs\bokbutik-main\src\dbconnect.php');
+require('../../src/dbconnect.php');
 //echo "<pre>";
-//print_r($_POST);    
+//print_r($_POST);
 //echo "</pre>";
 if(isset($_GET['updateSucces'])){
   $succesMessage = '
@@ -11,17 +12,17 @@ if(isset($_GET['updateSucces'])){
 ';
 }
 if (isset($_POST['deleteProductBtn'])) {
-    
+
     $sql = "
         DELETE FROM products
         WHERE id = :id;
     ";
     $stmt = $dbconnect->prepare($sql);
     $stmt->bindParam(":id", $_POST['productId']);
-    $stmt->execute(); 
-} 
+    $stmt->execute();
+}
 if (isset($_POST['addProductBtn'])) {
-	
+
 	$sql = "
 	   INSERT INTO products (title, description, price, stock,img_url)
 	   VALUES (:title, :description, :price, :stock, :img_url);
@@ -32,15 +33,15 @@ if (isset($_POST['addProductBtn'])) {
 	$stmt->bindParam(":price", $_POST['price']);
 	$stmt->bindParam(":stock", $_POST['stock']);
   $stmt->bindParam(":img_url", $_POST['img_url']);
-  $stmt->execute(); 
-	
-	
+  $stmt->execute();
+
+
 }
 
 
 
-$stmt       = $dbconnect->query("SELECT * FROM products"); 
-$products  = $stmt->fetchAll();                      
+$stmt       = $dbconnect->query("SELECT * FROM products");
+$products  = $stmt->fetchAll();
 echo "<pre>";
 print_r($products);
 echo "</pre>";
@@ -74,31 +75,31 @@ echo "</pre>";
 			   <th>Price</th>
 			   <th>Stock</th>
          <th>Img_url</th>
-			   
-			   
+
+
 		   </tr>
 	   </thead>
 
 	   <tbody>
-		   <?php 
-			   
+		   <?php
+
           foreach ($products as $product) { ?>
           <tr>
-              
+
               <td><?=htmlentities($product['title']) ?></td>
               <td><?=htmlentities($product['description']) ?></td>
               <td><?=htmlentities($product['price']) ?></td>
               <td><?=htmlentities($product['stock']) ?></td>
               <td><?=htmlentities($product['img_url']) ?></td>
-              
-              
+
+
               <td>
               <form action="" method="POST">
                       <input type="hidden" name="productId" value="<?=$product['id'] ?>">
                       <input type="submit" name="deleteProductBtn" value="Delete"><br></form>
-                  
-                      
-            
+
+
+
             <form action="update-product.php" method="GET">
                 <input type="submit" value="Update">
                 <input type="hidden" name="productId" value="<?= htmlentities($product['id']) ?>">
@@ -108,7 +109,7 @@ echo "</pre>";
         <?php }?>
   </tbody>
 </table>
-   
+
    <form action="" method="POST">
 	   <input type="text" name="title" placeholder="Title"><br>
 	   <input type="text" name="description" placeholder="Description"><br>
@@ -118,13 +119,13 @@ echo "</pre>";
 	   <input type="submit" name="addProductBtn" value="Add product"><br>
    </form>
 
-   
+
 </body>
 </html>
 
 
 
 
-	
+
 </body>
 </html>
--- public/admin/update-product.php
+++ public/admin/update-product.php
@@ -1,36 +1,36 @@
-<?php require('C:\MAMP\htdocs\bokbutik-main\src\dbconnect.php');
-require('')
+<?php
+//require('C:\MAMP\htdocs\bokbutik-main\src\dbconnect.php');
+require('../../src/dbconnect.php');
 
 
-
+$productId = $_GET['productId'];
 // Felmeddelande sätts till tomt
-$message = "";
+// $message = "";
 // Uppdatera användaruppgift
-if(isset($_POST['updateProductBtn'])) {
-  
+if (array_key_exists('updateAccountBtn', $_POST)) {
 
-    $sql = "
-    UPDATE products 
-    SET 
-      title  = :title,
-      description   = :description,
-      price       = :price,
-      stock    = :stock,
-      img_url       = :img_url,
-      
-    
-    WHERE id = :id
+
+  $sql = "
+    UPDATE products
+    SET
+      title=:title,
+      description=:description,
+      price=:price,
+      stock=:stock,
+      img_url=:img_url
+    WHERE id =:id
     ";
-    $statement = $dbconnect->prepare($sql);
-    $statement->bindParam(':id', $_GET['productId']);
-    $statement->bindParam(':title', $_POST['title']);
-    $statement->bindParam(':price', $_POST['price']);
-    $statement->bindParam(':stock', $_POST['stock']);
-    $statement->bindParam(':img_url', $_POST['img_url']);
-    $statement->execute();
+  $statement = $dbconnect->prepare($sql);
+  $statement->bindParam(':id', $productId);
+  $statement->bindParam(':title', $_POST['title']);
+  $statement->bindParam(':price', $_POST['price']);
+  $statement->bindParam(':description', $_POST['description']);
+  $statement->bindParam(':stock', $_POST['stock']);
+  $statement->bindParam(':img_url', $_POST['img_url']);
+  $statement->execute();
 
-    redirect("http://localhost/bokbutik-main/public/admin/index.php", "updateSucces");
-  }
+  if ($sql) header('Location: http://localhost/bokbutik-main/public/admin/index.php') ;
+}
 
 
 
@@ -39,7 +39,7 @@ if(isset($_POST['updateProductBtn'])) {
 
 // Hämtar en användaruppgift
 $sql = "
-      SELECT * FROM products 
+      SELECT * FROM products
       WHERE id = :id
 ";
 $statement = $dbconnect->prepare($sql);
@@ -47,49 +47,66 @@ $statement->bindParam(':id', $_GET['productId']);
 $statement->execute();
 $product = $statement->fetch();
 
- //echo "Product";
- //echo "<pre>";
- //print_r($product);
- //echo "</pre>";
+//echo "Product";
+//echo "<pre>";
+//print_r($product);
+//echo "</pre>";
 
 
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
 
-<div class="wrapper-register">
-  <h1>Uppdatera </h1>
-  </div>
-  <?=$message ?>
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
 
-  	
-  <form method="POST" action="" class="form mx-auto" >
-		
-		<div class="mb-3">
-    <label for="title" class="form-label">Title</label>
-    <input type="text" class="form-control" id="title" name="title" value="<?= htmlentities($product['title']) ?> ">
-  </div>
-  <div class="mb-3">
-    <label for="description" class="form-label">Description</label>
-    <input type="description" class="form-control" id="description" name="description" value="<?= htmlentities($product['description']) ?>">
-  </div>
-		
-		<div class="mb-3">
-    <label for="price" class="form-label">Price</label>
-    <input type="text" class="form-control" id="price" name="price" value="<?= htmlentities($product['price']) ?>">
-  </div>
-	
-  <div class="mb-3">
-    <label for="stock" class="form-label">Stock</label>
-    <input type="stock" class="form-control" name="stock" value="<?= htmlentities($product['stock']) ?>">
-  </div>
-	
-  <div class="mb-3">
-    <label for="img_url" class="form-label">Img_url</label>
-    <input type="img_url" class="form-control" id="img_url" name="img_url" value="<?= htmlentities($product['img_url']) ?>">
-  </div>
-	
-		
-  <!-- Update Btn -->
-  <input type="submit" class="btn btn-primary btn-form" name="updateAccountBtn" value="Uppdatera">
+<body>
 
-</form>
+
+
+  <div class="wrapper-register">
+    <h1>Uppdatera </h1>
+  </div>
+  <?= $message ?>
+
+
+  <form method="POST" class="form mx-auto">
+
+    <div class="mb-3">
+      <label for="title" class="form-label">Title</label>
+      <input type="text" class="form-control" id="title" name="title" value="<?= htmlentities($product['title']) ?> ">
+    </div>
+    <div class="mb-3">
+      <label for="description" class="form-label">Description</label>
+      <input type="description" class="form-control" id="description" name="description" value="<?= htmlentities($product['description']) ?>">
+    </div>
+
+    <div class="mb-3">
+      <label for="price" class="form-label">Price</label>
+      <input type="text" class="form-control" id="price" name="price" value="<?= htmlentities($product['price']) ?>">
+    </div>
+
+    <div class="mb-3">
+      <label for="stock" class="form-label">Stock</label>
+      <input type="stock" class="form-control" name="stock" value="<?= htmlentities($product['stock']) ?>">
+    </div>
+
+    <div class="mb-3">
+      <label for="img_url" class="form-label">Img_url</label>
+      <input type="img_url" class="form-control" id="img_url" name="img_url" value="<?= htmlentities($product['img_url']) ?>">
+    </div>
+
+
+    <!-- Update Btn -->
+    <input type="submit" class="btn btn-primary btn-form" name="updateAccountBtn" value="Uppdatera">
+
+  </form>
+
+</body>
+
+</html>
