b4c46d1c4481a53ce1ea121dcf5b331149c4d14f
CGranzell <91188714+CGranzell@users.noreply.github.com>
2022-06-09 06:17:46 +0200

    fixade tillbaka css länk i header

--- public/layout/header.php
+++ public/layout/header.php
@@ -7,13 +7,13 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-  
+  <link rel="stylesheet" href="<?=CSS_PATH . 'style.css'?>">
 
 
   <title>Header</title>
 </head>
 
-<body id="body">
+<body>
 
   <nav class="navbar-light" style="background-color: #e3f2fd;">
     <ul class="nav justify-content-center">
@@ -57,9 +57,3 @@
   </nav>
 
 
-
-  <!-- Bootstrap JS -->
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
-</body>
-
-</html>
